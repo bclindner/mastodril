4d9b418ffa3f7590858f1a3e03aa1b058fbae318
Brian C. Lindner <brian@bclindner.com>
2018-11-22 00:37:11 -0500

    initial commit with first working version

---
+++ go.mod
@@ -0,0 +1,14 @@
+module .
+
+require (
+	github.com/cenkalti/backoff v2.0.0+incompatible // indirect
+	github.com/dghubble/go-twitter v0.0.0-20181118100457-22773114c241 // indirect
+	github.com/dghubble/oauth1 v0.5.0 // indirect
+	github.com/dghubble/sling v1.2.0 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/gorilla/websocket v1.4.0 // indirect
+	github.com/mattn/go-mastodon v0.0.2 // indirect
+	github.com/tomnomnom/linkheader v0.0.0-20180905144013-02ca5825eb80 // indirect
+	golang.org/x/net v0.0.0-20181114220301-adae6a3d119a // indirect
+	golang.org/x/oauth2 v0.0.0-20181120190819-8f65e3013eba // indirect
+)
---
+++ mastodril.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"context"
+	"github.com/mattn/go-mastodon"
+	"github.com/dghubble/oauth1"
+	"github.com/dghubble/go-twitter/twitter"
+	"fmt"
+	"encoding/json"
+	"io/ioutil"
+	"encoding/binary"
+	"os"
+)
+
+type Config struct {
+	Username string `json:"handle"`
+	Twitter TwitterConfig `json:"twitter"`
+	Mastodon MastodonConfig `json:"mastodon"`
+}
+
+type TwitterConfig struct {
+	ConsumerKey string `json:"consumerKey"`
+	ConsumerSecret string `json:"consumerSecret"`
+	AccessToken string `json:"accessToken"`
+	AccessSecret string `json:"accessSecret"`
+}
+
+type MastodonConfig struct {
+	Server string `json:"server"`
+	ClientID string `json:"clientID"`
+	ClientSecret string `json:"clientSecret"`
+	AccessToken string `json:"accessToken"`
+}
+
+func GetTwitterClient (config TwitterConfig) *twitter.Client {
+	// setup oauth1 stuff
+	oauth := oauth1.NewConfig(config.ConsumerKey, config.ConsumerSecret)
+	token := oauth1.NewToken(config.AccessToken, config.AccessSecret)
+	// create the client
+	httpclient := oauth.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpclient)
+	// return it
+	return client
+}
+
+func GetMastodonClient(config MastodonConfig) *mastodon.Client {
+	// map MastodonConfig to an actual proper mastodon.Config, run NewClient with it
+	client := mastodon.NewClient(&mastodon.Config{
+		Server: config.Server,
+		ClientID: config.ClientID,
+		ClientSecret: config.ClientSecret,
+		AccessToken: config.AccessToken,
+	})
+	// return the result
+	return client
+}
+
+func main () {
+	// get the last checked tweet
+	var lastTweetID int64 = 0
+	lastTweetFile, err := ioutil.ReadFile("mastodril.last")
+	if err == nil {
+		lastTweetID, _ = binary.Varint(lastTweetFile)
+	}
+	// get config file
+	configFile, err := ioutil.ReadFile("mastodril.json")
+	if err != nil { fmt.Println(err); os.Exit(1) }
+	// parse the config
+	var config Config
+	err = json.Unmarshal(configFile, &config)
+	if err != nil { fmt.Println(err); os.Exit(1) }
+	// get api clients
+	t := GetTwitterClient(config.Twitter)
+	m := GetMastodonClient(config.Mastodon)
+	// get twitter timeline starting at the user in question
+	tweets, _, err := t.Timelines.UserTimeline(&twitter.UserTimelineParams{
+		ScreenName: config.Username,
+		SinceID: lastTweetID,
+		Count: 5,
+	})
+	if err != nil { fmt.Println(err); os.Exit(1) }
+	// post each tweet since the last checked tweet ID
+	// done in reverse order to keep chronological order
+	for i := len(tweets)-1; i >= 0; i-- {
+		tweet := tweets[i]
+		fmt.Println("found new tweet with id",tweet.IDStr)
+		m.PostStatus(context.Background(), &mastodon.Toot{
+			Status: tweet.Text,
+		})
+		lastTweetID = tweet.ID
+	}
+	// write the last tweet ID to the .last file
+	lastIDBinary := make([]byte, binary.MaxVarintLen64)
+	binary.PutVarint(lastIDBinary, lastTweetID)
+	err = ioutil.WriteFile("mastodril.last", lastIDBinary, os.FileMode(int(0644)))
+	if err != nil { fmt.Println(err); os.Exit(1) }
+	// done!
+}
